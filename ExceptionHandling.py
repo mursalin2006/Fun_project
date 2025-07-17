0dc80b3f255992fbfe668c0caca68c846790fc44
Mohammed Mursalin <mursalinchy17@gmail.com>
2025-07-17 11:02:53 +0600

    First working version of the project

--- ExceptionHandling.py
+++ ExceptionHandling.py
@@ -2,9 +2,9 @@ x = input('Enter first number:')
 y = input('Enter second number:')
 try:
     z = float(x) / float(y)
-except Exception as e:
-    print('Exception Type:',type(e).__name__)
-    z = None
-'''except ZeroDivisionError as e:
+except ZeroDivisionError as e:
     print('Exception Type:ZeroDivisionError')
-    z = None'''
+    z = None
+    '''except Exception as e:
+        print('Exception Type:',type(e).__name__)
+        z = None'''
